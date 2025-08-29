ed15ccba502e7e828252b7e7e36459617e41e554
NorViking <98680593+NorViking@users.noreply.github.com>
2025-08-29 10:56:05 +0000

    version 1.3.0.0 - changed css

--- src/extensions/spfxHideElements/SpfxHideElementsApplicationCustomizer.ts
+++ src/extensions/spfxHideElements/SpfxHideElementsApplicationCustomizer.ts
@@ -7,5 +7,12 @@ style.innerHTML = `
   .ModalFocusTrapZone3 {
     background-color: #FFFFFF !important;
   }
+
+  .OneUpModal_df49a5a0 div.OneUpRootAnimation_df49a5a0.OneUpEntranceV2_df49a5a0 {
+    visibility: hidden !important;
+  }
+  ..OneUpModal_df49a5a0 .OneUpRootDarkBackground_df49a5a0 {
+    background-color: #FFFFFF !important;
+  }    
 `;
 document.head.appendChild(style);
